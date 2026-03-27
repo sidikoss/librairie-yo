1dbf97bd45f740d2f35d2b12a5dd210ec1a23007
sidikoss <sidikouzekaba@gmail.com>
2026-03-27 02:02:44 +0000

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue, set, push, remove, update } from "firebase/database";
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
